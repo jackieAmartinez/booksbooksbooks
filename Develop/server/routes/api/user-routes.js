3dcb4525aeb21b8b9b311fe59b6cb7e998c2980d
Chester Springwood <jackieAmartinez@gmail.com>
2023-07-24 22:35:49 -0400

    routes and routes

--- Develop/server/routes/api/user-routes.js
+++ Develop/server/routes/api/user-routes.js
@@ -3,20 +3,14 @@ const {
   createUser,
   getSingleUser,
   saveBook,
-  deleteBook,
+  delBook,
   login,
 } = require('../../controllers/user-controller');
 
-// import middleware
 const { authMiddleware } = require('../../utils/auth');
 
-// put authMiddleware anywhere we need to send a token for verification of user
 router.route('/').post(createUser).put(authMiddleware, saveBook);
-
 router.route('/login').post(login);
-
 router.route('/me').get(authMiddleware, getSingleUser);
-
-router.route('/books/:bookId').delete(authMiddleware, deleteBook);
-
+router.route('/books/:bookId').delete(authMiddleware, delBook);
 module.exports = router;
